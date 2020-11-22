ef7a21f8434ac375dc11c548fe3b1075867ef590
tk1351 <takakikitsui0111@gmail.com>
2020-11-22 16:26:15 +0900

    setting

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const http = require('http')
+
+const server = http.createServer(app)
+const PORT = process.env.PORT || 8080
+const config = require('./config/dev')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('test')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+mongoose.connect(config.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+})
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.log('DB connected')
+})
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+server.listen(PORT, (error) => {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info('listen: ', PORT)
+  }
 })
