7a1ac6777b1c438d9fe5735fae99483ff7542cbe
tk1351 <takakikitsui0111@gmail.com>
2020-12-26 14:11:33 +0900

    confrim ci works

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,31 +1,18 @@
 const { MongoMemoryServer } = require('mongodb-memory-server')
 const mongoose = require('mongoose')
 
-// const mongod = new MongoMemoryServer({
-//   autoStart: true,
-//   debug: true,
-// })
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000
 
 module.exports.connect = async () => {
   mongod = await new MongoMemoryServer({ binary: { version: '4.0.14' } })
   const uri = await mongod.getUri()
 
-  // const mongooseOpts = {
-  //   useNewUrlParser: true,
-  //   autoReconnect: true,
-  //   reconnectTries: Number.MAX_VALUE,
-  //   reconnectInterval: 1000,
-  // }
-
   await mongoose.connect(uri, { useNewUrlParser: true })
   mongoose.set('debug', true)
 }
 
 module.exports.closeDB = async () => {
-  // mongod = new MongoMemoryServer()
   await mongoose.connection.dropDatabase()
-  // await mongoose.disconnect()
   await mongoose.connection.close()
   await mongod.stop()
 }
