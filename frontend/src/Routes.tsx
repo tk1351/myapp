f452daa7abda800e0066e4853f8a268983a9bb70
tk1351 <takakikitsui0111@gmail.com>
2020-12-31 10:40:41 +0900

    basename追加

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route, HashRouter } from 'react-router-dom'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import AddPost from './components/AddPost'
 import Feed from './components/Feed'
@@ -21,25 +21,25 @@ const Routes: React.FC = () => {
   const q = useSelector((state: any) => state.queryParams).slice(-1)[0].values.q
 
   return (
-      <HashRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/feed" component={Feed} />
-          <Route exact path="/add" component={AddPost} />
-          <Route exact path="c" component={Auth} />
-          <Route exact path={`/search?q=${q}`} component={SearchResult} />
-          <Route exact path={'/post/detail/:id'} component={SinglePostPage} />
-          <Route exact path="/post/edit/:id" component={EditPost} />
-          <Route exact path="/user/profile/:id" component={UserProfile} />
-          <Route exact path="/user/edit/:id" component={EditProfile} />
-          <Route exact path="/category/:id" component={CategoryPage} />
-          <Route exact path="/admin" component={AdminScreen} />
-          <Route exact path="/admin/user" component={AdminUsersList} />
-          <Route exact path="/admin/post" component={AdminPostsList} />
-          <Route exact path="/admin/post/:id" component={AdminPostDetail} />
-          <Route component={NotFound} />
-        </Switch>
-      </HashRouter>
+    <BrowserRouter basename="/">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/feed" component={Feed} />
+        <Route exact path="/add" component={AddPost} />
+        <Route exact path="c" component={Auth} />
+        <Route exact path={`/search?q=${q}`} component={SearchResult} />
+        <Route exact path={'/post/detail/:id'} component={SinglePostPage} />
+        <Route exact path="/post/edit/:id" component={EditPost} />
+        <Route exact path="/user/profile/:id" component={UserProfile} />
+        <Route exact path="/user/edit/:id" component={EditProfile} />
+        <Route exact path="/category/:id" component={CategoryPage} />
+        <Route exact path="/admin" component={AdminScreen} />
+        <Route exact path="/admin/user" component={AdminUsersList} />
+        <Route exact path="/admin/post" component={AdminPostsList} />
+        <Route exact path="/admin/post/:id" component={AdminPostDetail} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   )
 }
 
